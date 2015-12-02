3a980e098f8e82312188ab90d1a7403a2263c94d
James Guillochon <jfg@ucolick.org>
2015-12-02 11:31:51 -0800

    Major rewrite of event format.

--- bandmag.F90
+++ bandmag.F90
@@ -233,7 +233,7 @@ subroutine bandmag(times, fbs, mdots, bands, mags, penalties, routs, rphots)
         if (reprocess_model .ne. RM_NONE .and. .not. circ_phot) then
             call dmdt([dftime], dm, .false., im2)
 
-            dfcovering = trial_opacity(cur_event)*max(0.d0,im2(1))
+            dfcovering = trial_opacity(cur_event)!*max(0.d0,im2(1))
 
             ! If the light escape time is comparable to the time, exclude this
             ! solution, as it would delay the light curve.
@@ -241,7 +241,7 @@ subroutine bandmag(times, fbs, mdots, bands, mags, penalties, routs, rphots)
             !    penalties(j) = 1
             !endif
 
-            dfcovering = 1.d0 - dexp(-dfcovering)
+            !dfcovering = 1.d0 - dexp(-dfcovering)
 
             if (df_rphot .lt. dfri) then
                 penalties(j) = 1

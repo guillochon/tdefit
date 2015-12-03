516e1684ffaba0f6bfc2ca72b7646342c6372ac7
James Guillochon <guillochon@gmail.com>
2015-12-03 00:26:31 -0500

    Now tolerate blank lines in both event and parameter loads.

--- load_event.F90
+++ load_event.F90
@@ -51,6 +51,8 @@ subroutine load_event(e, prepare)
     blri = 0
     linedo: do
         read(fn, '(A500)', iostat=stat, size=nbuf, advance='no') buffer
+        if (stat .lt. 0) exit
+        if (stat .gt. 0) cycle
         if (is_iostat_eor(stat)) then
             j = 0
             loc = 1
